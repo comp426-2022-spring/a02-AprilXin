9b3a7d28c6d92078a4c7531d8e0140bc1e1f8f1f
AprilXin <shuning@live.unc.edu>
2022-02-10 22:32:45 -0500

    require

--- flips.js
+++ flips.js
@@ -1,4 +1,7 @@
 import { coinFlips, countFlips } from "./modules/coin.mjs";
-const number = process.argv.slice(2);
+//const number = process.argv.slice(2);
+const args = require("minimist")(process.argv.slice(2));
+args ["number"];
+const number = args.number || 1;
 const flips = coinFlips(number);
 console.log(flips, countFlips(flips));
