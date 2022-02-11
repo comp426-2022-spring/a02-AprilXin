4e11a51a6d92844b8bcda2d7198fc90d93cd953f
AprilXin <shuning@live.unc.edu>
2022-02-11 14:48:29 -0500

    change args

--- flips.js
+++ flips.js
@@ -1,7 +1,5 @@
 import { coinFlips, countFlips } from "./modules/coin.mjs";
-//const number = process.argv.slice(2);
-const args = require("minimist")(process.argv.slice(2));
-args ["number"];
-const number = args.number || 1;
+const args = process.argv.slice(2)[0].slice(9);
+const number = args || 1;
 const flips = coinFlips(number);
 console.log(flips, countFlips(flips));
--- guess-flip.js
+++ guess-flip.js
@@ -1,5 +1,5 @@
 import { flipACoin } from "./modules/coin.mjs";
-let call = process.argv.slice(2);
+let call = process.argv.slice(2)[0].slice(7);
 if (call != "heads" && call != "tails") {
     console.error("Wrong input.");
 } else {
