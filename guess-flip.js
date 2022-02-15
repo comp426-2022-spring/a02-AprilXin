bb2c20901f9a849af01c0c57fcb920a7d7f92c72
AprilXin <shuning@live.unc.edu>
2022-02-14 20:35:17 -0500

    change guess error

--- guess-flip.js
+++ guess-flip.js
@@ -4,8 +4,10 @@ const require = createRequire(import.meta.url);
 const args = require("minimist")(process.argv.slice(2));
 args["call"];
 const call = args.call;
-if (call != "heads" && call != "tails") {
-    console.error("Wrong input.");
+if (call == null) {
+    console.error("Error: no input.");
+} else if (call != "heads" && call != "tails") {
+    console.error("Usage: node guess-flip --call=[heads|tails]");
 } else {
     console.log(flipACoin(call));
 }
