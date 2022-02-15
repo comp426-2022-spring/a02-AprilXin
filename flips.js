ce82a58a7f223a6c789bca825e339355e1254daa
AprilXin <shuning@live.unc.edu>
2022-02-14 20:15:22 -0500

    test flips with no args

--- flips.js
+++ flips.js
@@ -1,8 +1,9 @@
 import { coinFlips, countFlips } from "./modules/coin.mjs";
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const args = require("minimist")(process.argv.slice(2));
-args["number"];
-const number = args.number || 1;
+// import { createRequire } from 'module';
+// const require = createRequire(import.meta.url);
+// const args = require("minimist")(process.argv.slice(2));
+// args["number"];
+// const number = args.number || 1;
+const number = 1;
 const flips = coinFlips(number);
 console.log(flips, countFlips(flips));
